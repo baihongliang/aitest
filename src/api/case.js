bc229351c3cf08b3efd7a59e930d2f1c30be3bac
baihongliang <18211097157@139.com>
2021-03-22 09:43:15 +0800

    完成测试用例的删除功能

--- src/api/case.js
+++ src/api/case.js
@@ -8,7 +8,14 @@ const cases = {
         return axios.post('/testCase/text', params)
     },
     creatCaseByFile(params){
-        return axios.post('/testCase/file', params)
+        return axios('/testCase/file', {
+            method:'post',
+            data:params,
+            Headers:{'Cotent-type':'multipart/form-data'}
+        })
+    },
+    deleteCase(params){
+        return axios.delete('/testCase/'+params.caseId, params)
     }
 }
 export default cases
--- src/components/Case.vue
+++ src/components/Case.vue
@@ -21,7 +21,8 @@
                 <v-container>
                     <v-text-field label="用例名称" v-model="addItem.name"></v-text-field>
                     <v-select :items="selectItem" v-model="addItem.type" label="用例类型"></v-select>
-                    <v-textarea label="用例数据" v-model="addItem.data"></v-textarea>
+                    <v-textarea label="用例数据" v-model="addItem.data" v-if="addItem.type=='文本'"></v-textarea>
+                    <v-file-input label="用例数据" v-model="addItem.file" v-if="addItem.type=='文件'"></v-file-input>
                     <v-text-field label="备注" v-model="addItem.remark"></v-text-field>
                 </v-container>
             </v-card-text>
@@ -40,13 +41,13 @@
                 v-model="selected"
                 :headers="headers"
                 :items="desserts"
-                item-key="name"
+                item-key="id"
                 show-select
                 class="elevation-1"
             >
             <template v-slot:[`item.operate`] = "{item}">
                 <v-btn color="primary" text small>编辑</v-btn>
-                <v-btn color="error" text small>删除</v-btn>
+                <v-btn color="error" text small @click="deleteCase(item)">删除</v-btn>
 
             </template>
             </v-data-table>
@@ -64,6 +65,7 @@ export default {
                 name:'',
                 type:'',
                 data:'',
+                file:'',
                 remark:''
             },
             selected:[],
@@ -100,15 +102,62 @@ export default {
     },
     methods: {
         addCase(){
-            console.log(this.addItem)
-            let post_data = {
-                caseData: this.addItem.data,
-                caseName: this.addItem.name,
-                remark: this.addItem.remark,
+            if(this.addItem.type=='文本'){
+                let post_data = {
+                    caseData: this.addItem.data,
+                    caseName: this.addItem.name,
+                    remark: this.addItem.remark,
+                }
+                this.$api.cases.creatCaseByText(post_data).then(res=>{
+                    console.log(res)
+                    let post_data = {
+                        pageNum:1,
+                        pageSize:10,
+                    }
+                    // 添加用例后，刷新用例列表。注意：由于上传文件后端处理较慢，并没有及时生成新的用例
+                    this.$api.cases.getList(post_data).then(res=>{
+                        console.log(res)
+                        this.desserts = res.data.data.data
+                    })
+                })
+            } else if(this.addItem.type=='文件'){
+                let post_data = new FormData()
+                post_data.append('caseFile',this.addItem.file)
+                post_data.append('caseData',this.addItem.data)
+                post_data.append('caseName',this.addItem.name)
+                post_data.append('remark',this.addItem.remark)
+                this.$api.cases.creatCaseByFile(post_data).then(res=>{
+                    console.log(res)
+                    let post_data = {
+                        pageNum:1,
+                        pageSize:10,
+                    }
+                    // 添加用例后，刷新用例列表。注意：由于上传文件后端处理较慢，并没有及时生成新的用例
+                    this.$api.cases.getList(post_data).then(res=>{
+                        console.log(res)
+                        this.desserts = res.data.data.data
+                    })
+                })
             }
-            this.$api.cases.creatCaseByText(post_data).then(res=>{
+            console.log(this.addItem)
+            this.addDialog = false
+        },
+        deleteCase(item){
+            console.log(item)
+            let post_data = {
+                caseId:item.id
+            }
+            this.$api.cases.deleteCase(post_data).then(res=>{
                 console.log(res)
-                this.addDialog = false
+                let post_data = {
+                    pageNum:1,
+                    pageSize:10,
+                }
+                // 添加用例后，刷新用例列表。注意：由于上传文件后端处理较慢，并没有及时生成新的用例
+                this.$api.cases.getList(post_data).then(res=>{
+                    console.log(res)
+                    this.desserts = res.data.data.data
+                })
             })
         }
     },
